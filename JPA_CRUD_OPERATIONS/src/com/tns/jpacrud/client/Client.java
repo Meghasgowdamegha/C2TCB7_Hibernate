d46356671f03fae77abcbc7fef29faf7f66be959
Meghasgowdamegha <96436236+Meghasgowdamegha@users.noreply.github.com>
2022-03-07 08:51:52 +0530

    First commit

---
+++ JPA_CRUD_OPERATIONS/src/com/tns/jpacrud/client/Client.java
@@ -0,0 +1,48 @@
+package com.tns.jpacrud.client;
+
+import com.tns.jpacrud.entities.Student;
+import com.tns.jpacrud.service.StudentService;
+import com.tns.jpacrud.service.StudentServiceImpl;
+
+public class Client {
+	
+public static void main(String[] args) {
+//Debug this program as Debug -> Debug as Java 
+//Application
+
+StudentService service = new StudentServiceImpl();
+Student student = new Student();
+
+// Create Operation
+/*
+ * student.setStudentId(4); student.setName("Sanvi");
+ * service.addStudent(student);
+ */
+
+
+//Retrieve Operation
+student = service.findStudentById(4);
+System.out.print("ID:"+student.getStudentId());
+System.out.println(" Name:"+student.getName()); 
+
+//Update Operation
+student = service.findStudentById(4);
+student.setName("M Sanvi");
+service.updateStudent(student);
+
+//at this breakpoint, we have added one record to table
+//Retrieve Operation
+student = service.findStudentById(4);
+System.out.print("ID:"+student.getStudentId());
+System.out.println(" Name:"+student.getName()); 
+
+
+//at this breakpoint, record is removed from table
+//Delete Operation
+student = service.findStudentById(4);
+service.removeStudent(student);
+System.out.println("End of program/Life cycle completed..."); 
+
+
+  }
+}
